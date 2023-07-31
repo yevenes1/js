5584af278c850e923475bdc6008614ed47b84a0a
yevenes1 <yevenesdev@gmail.com>
2023-07-30 23:02:40 -0400

    fix github error

---
+++ js/products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "DragonLore",
+        precio: 180,
+        img:
+            "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot621FAR17P7NdTRH-t26q4SZlvD7PYTQgXtu5Mx2gv3--Y3nj1H6qBFvMWHyIo7Adw9raF6GrlK9lLi-jJO7tJ_JzyNh63F3sX_emUPkgQYMMLL2MDRAbQ/360fx360f"
+    },
+    {
+        id: 2,
+        nombre: "Craneos de Cobalto",
+        precio: 90,
+        img:
+            "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DfVlxgLQFFibKkJQN3wfLYYgJK7dKyg5KKh8j4NrrFnm5D8fp3i-vT_I_KilihriwvOCyveMX6L1NqOB2NlQ3vg7m6m5To6JnOynRkvCUis3zVn0PmgxFPO-ds0_ydQVzPBPcaTfSBAyeC4p9tCHL9GotlaTc/360fx360f"
+    },
+    {   id: 3,
+        nombre: "Vendas de Cuero",
+        precio: 25,
+        img: 
+            "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DfVlxgLQFFibKkJQN3wfLYYgJK7dKyg5KKh8j4NrrFnm5D8fp5j-jX7LP5iUazrl1sYGuhLYKUewE_aV6DrwK-x-rv0cfovcydnSM3vCcj4i7cy0bliRsYcKUx0uIx_ZsL/360fx360f"    
+    },
+    {
+        id: 4,
+        nombre: "Stilete",
+        precio: 40,
+        img:
+            "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQh5hlcX0nvUOGsx8DdQBJjIAVHubSaIAlp1fb3YilL4NmigI_FxvbxNb6FlWoB7ZEi2e_CrY-mjg3m_xE9YG_xJo6WJwY4Z16C_we2lfCv28HnEtd6yQ/360fx360f"  
+    },
+    {
+        id: 5,
+        nombre:"Tela Escarlata",
+        precio: 100,
+        img: 
+            "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJf2PLacDBA5ciJnJm0gPL2IITck29Y_chOhujT8om7iwLn_Ec4NWrwdoDDIFNtZlHT-QW6xOzmgZ_t6pTNznUyvnQg7Crclwv330__CL9_dg/360fx360f"
+    }
+
+];
--- js/script.js
+++ js/script.js
@@ -1,17 +1,75 @@
-function submitForm(event) {
-  event.preventDefault(); 
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
 
- 
-  let name = document.getElementById('name').value;
-  let email = document.getElementById('email').value;
-  let message = document.getElementById('message').value;
 
-  if (name === '' || email === '' || message === '') {
-    console.log('Por favor, completa todos los campos del formulario.');
-    return; 
-  }
+let carrito = [];
 
-  
-  console.log('¡El formulario ha sido enviado con éxito!');
-  document.getElementById('contact-form').reset();
-}
+productos.forEach((product) =>{
+    let content = document.createElement('div');
+    content.className = "card";
+    content.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="price">${product.precio} $</p>
+    `;
+
+    shopContent.append(content);
+
+    let comprar = document.createElement('button')
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
+
+    content.appendChild(comprar);
+
+
+    comprar.addEventListener("click", () =>{
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+        });
+        console.log(carrito);
+    });
+});
+
+verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+        <h1 class="modal-header-title">Carrito.</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "x";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "modal-content";
+        carritoContent.innerHTML = `
+        <img src="${product.img}">
+        <h3${product.nombre}</h3>
+        <p>${product.precio}</p>
+    `;
+
+    modalContainer.append(carritoContent)
+    });
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0); 
+    
+    const totalBuying = document.createElement('div');
+    totalBuying.className = "total-content"
+    totalBuying.innerHTML = `total a pagar: ${total} $`;
+    modalContainer.append(totalBuying);
+});
